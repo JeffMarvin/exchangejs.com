aeb78df487058d1040bf7f214919e4799f2a94e8
Mark Bennett <mark@burmis.ca>
2016-09-14 21:31:09 -0600

    Pull events from meetup.com

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var meetup = require('meetup-api')({
+	key: process.env.MEETUP_KEY
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {title: 'Exchange.js'});
+});
+
+router.get('/api/events', function(req, res) {
+  meetup.getEvents({
+    group_urlname: "startupedmonton"
+  }, function(error, events) {
+    if (error) {
+      res.json(error);
+    } else {
+      let events = events.results.filter(evt => evt.name.indexOf("Exchange.js") != -1);
+      res.json(events);
+    }
+  })
 });
 
 module.exports = router;
